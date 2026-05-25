aa66a996375c2ca279346e2570531c31049e6dfd
Werner Saboia <wernersaboia@gmail.com>
2026-05-24 22:00:26 -0300

    refactor: revisando dependências do projeto

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -4,13 +4,13 @@ import * as Sentry from "@sentry/nextjs";
 Sentry.init({
     dsn: process.env.NEXT_PUBLIC_SENTRY_DSN || process.env.SENTRY_DSN,
     environment: process.env.SENTRY_ENVIRONMENT || process.env.NODE_ENV || "development",
-    tracesSampleRate: 1.0,
+    tracesSampleRate: 0.1,
     replaysSessionSampleRate: 0.1,
     replaysOnErrorSampleRate: 1.0,
     integrations: [
         Sentry.replayIntegration({
-            maskAllText: false,
-            blockAllMedia: false,
+            maskAllText: true,
+            blockAllMedia: true,
         }),
     ],
 });
