74bbc67a95622ead120ddc89c73e40bfd38e9c1e
Werner Saboia <wernersaboia@gmail.com>
2026-06-01 21:07:50 -0300

    feat: implementando Sentry

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -2,7 +2,7 @@
 import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
-  dsn: process.env.SENTRY_DSN,
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN || process.env.SENTRY_DSN,
   environment: process.env.SENTRY_ENVIRONMENT || process.env.NODE_ENV || 'development',
-  tracesSampleRate: 1.0,
+  tracesSampleRate: 0.1,
 });
