92a6a13e8d84900fd291678159baa043d098c945
Werner Saboia <wernersaboia@gmail.com>
2026-06-19 16:13:29 -0300

    fix: resolvendo erro em pedidos anteriores do modo teste

--- next.config.ts
+++ next.config.ts
@@ -51,7 +51,7 @@ const nextConfig: NextConfig = {
               "default-src 'self'",
               "script-src 'self' 'unsafe-inline' js.stripe.com *.mercadopago.com browser.sentry-cdn.com *.unsplash.com",
               "frame-src 'self' js.stripe.com hooks.stripe.com www.paypal.com www.mercadopago.com.br",
-              "connect-src 'self' *.supabase.co api.stripe.com api.mercadopago.com api-m.paypal.com *.ingest.sentry.io",
+              "connect-src 'self' https://*.supabase.co wss://*.supabase.co api.stripe.com api.mercadopago.com api-m.paypal.com *.ingest.sentry.io",
               "img-src 'self' data: res.cloudinary.com images.unsplash.com *.supabase.co lh3.googleusercontent.com",
               "style-src 'self' 'unsafe-inline'",
               "font-src 'self'",
